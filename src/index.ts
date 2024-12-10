a80526296dcc4b4fc6e2aa3009f05c969525f31d
Thienan1092005 <ogyminecraft497@gmail.com>
2024-12-10 20:33:57 +0700

    add exprssjs and type for expressjs

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('123')
+import express from 'express'
+
+const port = 3000
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.json({
+    name: 'test',
+    author: 'Yuki'
+  })
+})
+
+app.listen(port, () => {
+  console.log(`server is running  on ${port}`)
+})
